41300aa97a2360699383324a1350268dcac03b5f
Kai Guo <guokai8@gmail.com>
2021-12-09 13:41:38 -0600

    Update loss.py

--- Loss/loss.py
+++ Loss/loss.py
@@ -1,34 +1,34 @@
 ''' Loss function for MSE '''
 ''' L = (y-y_hat)^2 '''
 def update_weights_MSE(a, b, X, Y, lr):
-    ad = 0
-    bd = 0
+    a_d = 0
+    b_d = 0
     N = len(X)
     for i in range(N):
         # 偏导数
         # -2x(y-(ax+b))
-        ad += -2*X[i]*(Y[i]-(a*X[i]+b))
+        a_d += -2*X[i]*(Y[i]-(a*X[i]+b))
         # -2(y-(ax+b))
-        bd += -2*(Y[i]-(a*X[i]+b))
+        b_d += -2*(Y[i]-(a*X[i]+b))
     # update a,b
-    a -= (a/float(N))*lr
-    b -= (b/float(N))*lr
+    a -= (a_d/float(N))*lr
+    b -= (b_d/float(N))*lr
     return(a,b)
 
 ''' Loss function for MAE '''
 ''' L = |y-y_hat| '''
 def update_weights_MAE(a,b,X,Y,lr):
-    ad = 0
-    bd = 0
+    a_d = 0
+    b_d = 0
     N = len(X)
     for i in range(N):
         # -x(y-(ax+b))/|ax+b|
-        ad += X[i]*(Y[i]-(a*X[i]+b))/abs(Y[i]-(a*X[i]+b))
+        a_d += X[i]*(Y[i]-(a*X[i]+b))/abs(Y[i]-(a*X[i]+b))
         # -(y-(ax+b))/|ax+b|
-        bd += -(Y[i]-(a*X[i]+b))/abs(Y[i]-(a*X[i]+b))
+        b_d += -(Y[i]-(a*X[i]+b))/abs(Y[i]-(a*X[i]+b))
     #update a,b
-    a -= (a/float(N))*lr
-    b -= (b/float(N))*lr
+    a -= (a_d/float(N))*lr
+    b -= (b_d/float(N))*lr
     return(a,b)
 ''' Loss function for Huber '''
 ''' L = 1/2(y-y_hat)^2, if |y-y_hat| <= delta
